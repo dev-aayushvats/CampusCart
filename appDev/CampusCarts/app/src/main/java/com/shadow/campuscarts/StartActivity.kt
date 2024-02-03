f93af07e167f5fc88d485a5ad7089fee1fdcf834
Anvay <anvayjaykar@gmail.com>
2024-02-03 23:59:26 +0530

    some app dev

---
+++ appDev/CampusCarts/app/src/main/java/com/shadow/campuscarts/StartActivity.kt
@@ -0,0 +1,11 @@
+package com.shadow.campuscarts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
